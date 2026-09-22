5ed7d9c66fdac8ff691b0d64167fd83909083ba9
MaximKolegov <ballbobba@gmail.com>
2026-09-22 20:43:53 +0300

    task2

--- script.js
+++ script.js
@@ -5,12 +5,14 @@
 
 const firstCard = document.querySelector('[data-event="talk"]');
 const labStatus = document.querySelector("#lab-status");
-
+const eventName = "Город в деталях";
+let clickCount = 0;
 // 01. Уберите /* и */ вокруг обработчика, сохраните файл и нажмите
 // первую карточку. Потом переходите к заданию 02 в TASKS.md.
 
 firstCard.addEventListener("click", () => {
-  labStatus.textContent = "Первая карточка доступна";
+  clickCount = clickCount + 1;
+  labStatus.textContent = `${eventName}: нажатий ${clickCount}`;
 });
 
 
